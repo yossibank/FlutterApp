4d0ee08560bb039080547d12f1ba659e6775a3c6
yossibank <yossibank@yahoo.co.jp>
2023-05-21 21:35:14 +0900

    PopupMenuButton, Slider作成

--- practice3/lib/test_page3.dart
+++ practice3/lib/test_page3.dart
@@ -10,11 +10,20 @@ class TestPage3 extends StatefulWidget {
 class _TestPage3State extends State<TestPage3> {
   static var _dropdownMessage = 'select: $_selected';
   static var _selected = 'One';
+  static var _sliderMessage = 'slider value $_value';
+  static var _value = 0.0;
 
   void popupSelected(String? value) {
     setState(() {
       _selected = value ?? 'not selected...';
       _dropdownMessage = 'select: $_selected';
+    });
+  }
+
+  void sliderChanged(double value) {
+    setState(() {
+      _value = value.floorToDouble();
+      _sliderMessage = 'slider value: $_value';
     });
   }
 
@@ -44,28 +53,75 @@ class _TestPage3State extends State<TestPage3> {
             const Padding(
               padding: EdgeInsets.all(10.0),
             ),
-            DropdownButton<String>(
-              items: const <DropdownMenuItem<String>>[
-                DropdownMenuItem<String>(
-                  value: 'One',
-                  child: Text('One'),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: DropdownButton<String>(
+                items: const <DropdownMenuItem<String>>[
+                  DropdownMenuItem<String>(
+                    value: 'One',
+                    child: Text('One'),
+                  ),
+                  DropdownMenuItem<String>(
+                    value: 'Two',
+                    child: Text('Two'),
+                  ),
+                  DropdownMenuItem<String>(
+                    value: 'Three',
+                    child: Text('Three'),
+                  ),
+                ],
+                onChanged: popupSelected,
+                value: _selected,
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: 'Roboto',
                 ),
-                DropdownMenuItem<String>(
-                  value: 'Two',
-                  child: Text('Two'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: PopupMenuButton(
+                  onSelected: (String value) => popupSelected(value),
+                  itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+                    const PopupMenuItem(
+                      value: 'One',
+                      child: Text('One'),
+                    ),
+                    const PopupMenuItem(
+                      value: 'Two',
+                      child: Text('Two'),
+                    ),
+                    const PopupMenuItem(
+                      value: 'Three',
+                      child: Text('Three'),
+                    ),
+                  ],
                 ),
-                DropdownMenuItem<String>(
-                  value: 'Three',
-                  child: Text('Three'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text(
+                _sliderMessage,
+                style: const TextStyle(
+                  fontSize: 32.0,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: 'Roboto',
                 ),
-              ],
-              onChanged: popupSelected,
-              value: _selected,
-              style: const TextStyle(
-                color: Colors.black,
-                fontSize: 28.0,
-                fontWeight: FontWeight.w400,
-                fontFamily: 'Roboto',
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Slider(
+                onChanged: sliderChanged,
+                min: 0.0,
+                max: 100.0,
+                divisions: 20,
+                value: _value,
               ),
             ),
           ],
